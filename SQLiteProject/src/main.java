2b38efead408da89bea793c92e30127b55f15cfc
kimshin <hanmo2000110@gmail.com>
2021-10-01 22:13:07 +0900

    SQliteProject 실

---
+++ SQLiteProject/src/main.java
@@ -0,0 +1,83 @@
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class main {
+	public static void main( String[] args ) {
+		Connection con = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			
+			String dbFile = "myfirst.db";
+			con = DriverManager.getConnection("jdbc:sqlite:" + dbFile);
+			
+			
+			
+//			System.out.println("*** 새 데이터 추가 ***");
+//			java.sql.Statement stat2 = con.createStatement();
+//			String sql2 = "insert into g_artists (name, a_type, a_year, debut, regdate)" +
+//			" values ('악뮤','혼성','2010년대, 2020년대','201?',datetime('now','localtime'));";
+//			int cnt = stat2.executeUpdate(sql2);
+//			if(cnt > 0)
+//				System.out.println("새로운 데이터가 추가되었습니다!");
+//			else 
+//				System.out.println("[Error] 데이터 오류!");
+//			
+//			stat2.close();
+		
+			
+			
+			
+//			
+//			System.out.println("*** 데이터 수정 ***");
+//			java.sql.Statement stat3 = con.createStatement();
+//			String sql3 = "update g_artists set name = '악동뮤지션' "+"where id=10 ;";
+//			int cnt3 = stat3.executeUpdate(sql3);
+//			if(cnt3 > 0)
+//				System.out.println("데이터가 수정되었습니다!");
+//			else 
+//				System.out.println("[Error] 데이터 수정 오류!");
+//			stat3.close();
+			
+			
+			
+			System.out.println("*** 데이터 삭제 ***");
+			java.sql.Statement stat4 = con.createStatement();
+			String sql4 = "delete from g_artists where id=6 ;";
+			int cnt4 = stat4.executeUpdate(sql4);
+			if(cnt4 > 0)
+				System.out.println("데이터가 삭제되었습니다!");
+			else 
+				System.out.println("[Error] 데이터 삭제 오류!");
+			stat4.close();
+			
+			
+			System.out.println("\n*** 데이터 조회 ***");
+			java.sql.Statement stat1 = con.createStatement();
+			String sql1 = "select * from g_artists";
+			ResultSet rs1 = stat1.executeQuery(sql1);
+			while(rs1.next()) {
+				String id = rs1.getString("id");
+				String name = rs1.getString("name");
+				System.out.println(id + " " + name);
+			}
+			stat1.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally {
+			if(con != null) {
+				try {
+					con.close();
+				}
+				catch(Exception e){}
+			}
+		}
+	}
+}
+
+
+
